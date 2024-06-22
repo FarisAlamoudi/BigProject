20ba6f25a447c3b5743506222180510581faee82
keatsane <keatingsane@outlook.com>
2024-06-22 01:25:10 -0400

    Messy but function endpoints for Users, Resources, and Reservations

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 require('express');
-const {ObjectId} = require('mongodb');
+const {ObjectId, ReturnDocument} = require('mongodb');
 
 /*  API Endpoints:
 
@@ -7,22 +7,23 @@ const {ObjectId} = require('mongodb');
     Resources: Add | Edit | Delete
     Reservations: Add | Edit | Delete
     ...
+    Email/phone verification
+    ...
     (Admins can modify all resources and reservations, users only modify owned)
 
-    NEEDS Json Web Token (JWT) stuff figured out
+    Needs Json Web Token (JWT) implemented
 
 */
 
 exports.setApp = function (app, client)
 {
-    // var token = require('./createJWT.js');
-
+    // might want to add password hashing?
     app.post('/api/register', async (req, res, next) =>
     {
-        // incoming: FirstName, LastName, Login, Password, Email
+        // incoming: FirstName, LastName, Login, Password, Email, Phone
         // outgoing: newUser || error
 
-        const {FirstName,LastName,Login,Password,Email} = req.body;
+        const {FirstName,LastName,Login,Password,Email,Phone} = req.body;
 
         try
         {
@@ -34,24 +35,13 @@ exports.setApp = function (app, client)
                 return res.status(409).json({error:'Username already exists.'});
             }
 
-            // might want to add password hashing and email/phone verification before inserting?
-
-            // if email/phone verified
-            const ret = await db.collection('Users').insertOne(
+            const insertedUser = await db.collection('Users').insertOne(
             {
-                FirstName:FirstName,
-                LastName:LastName,
-                Login:Login,
-                Password:Password,
-                Email:Email,
-                IsAdmin:false
+                FirstName,LastName,Login,Password,Email,Phone,
+                IsAdmin:false,EmailVerified:false,PhoneVerified:false
             });
 
-            // const tokenPayload = {userId:result._id};
-            // const token = jwt.sign(tokenPayload, jwtSecret, {expiresIn: '1h'});
-
-            // temporary return - should just return back to login page after correct email/phone verification
-            const newUser = await db.collection('Users').findOne({_id:ret.insertedId});
+            const newUser = await db.collection('Users').findOne({_id:insertedUser.insertedId});
             res.status(201).json(newUser);
         }
         catch (e)
@@ -61,6 +51,7 @@ exports.setApp = function (app, client)
         }
     });
 
+    // email/phone 2 factor authentication prior to login?
     app.post('/api/login', async (req, res, next) =>
     {
         // incoming: Login, Password
@@ -75,26 +66,18 @@ exports.setApp = function (app, client)
 
             if (user && Password === user.Password)
             {
-                // id = user[0]._id;
-                // fn = user[0].FirstName;
-                // ln = user[0].LastName;
-
-                // try
-                // {
-                //     const token = require("./createJWT.js");
-                //     user = token.createToken( fn, ln, id );
-                // }
-                // catch(e)
-                // {
-                //     user = {error:e.message};
-                // }
-
-                // send 2FA to email/phone
-                res.status(200).json(user);
+                if (user.EmailVerified || user.PhoneVerified)
+                {
+                    res.status(200).json(user);
+                }
+                else
+                {
+                    res.status(401).json({error:'Must verify email address or phone number.'});
+                }
             }
             else
             {
-                res.status(401).json({error:'Login/Password Incorrect.'});
+                res.status(401).json({error:'Login/Password incorrect.'});
             }
         }
         catch (e)
@@ -106,28 +89,10 @@ exports.setApp = function (app, client)
 
     app.post('/api/addresource', async (req, res, next) =>
     {
-        // incoming: UserID, Type, Start, End
+        // incoming: UserID, Type, Location, Description, Start, End
         // outgoing: newResource || error
 
-        // const {UserID, Type, Start, End, jwtToken} = req.body;
-        const {UserID, Type, Start, End} = req.body;
-
-        // try
-        // {
-        //     if (token.isExpired(jwtToken))
-        //     {
-        //         var r = {error:'The JWT is no longer valid', jwtToken: ''};
-        //         res.status(200).json(r);
-        //         return;
-        //     }
-        // }
-        // catch(e)
-        // {
-        //     console.log(e.message);
-        //     var r = {error:e.message, jwtToken: ''};
-        //     res.status(200).json(r);
-        //     return;
-        // }
+        const {UserID,Type,Location,Description,Start,End} = req.body;
 
         try
         {
@@ -139,16 +104,13 @@ exports.setApp = function (app, client)
                 return res.status(409).json({error:'Resource of this name already exists.'});
             }
 
-            const ret = await db.collection('Resources').insertOne({
-                // works but idk
-                UserID:new ObjectId(UserID),
-                Type:Type,
-                Start:new Date(Start),
-                End:new Date(End)
+            const insertedResource = await db.collection('Resources').insertOne(
+            {
+                UserID:ObjectId.createFromHexString(UserID),Type,Location,
+                Description,Start:new Date(Start),End:new Date(End)
             });
 
-            // temporary return - probably dont need to return anything
-            const newResource = await db.collection('Resources').findOne({_id:ret.insertedId});
+            const newResource = await db.collection('Resources').findOne({_id:insertedResource.insertedId});
             res.status(201).json(newResource);
         }
         catch (e)
@@ -156,52 +118,21 @@ exports.setApp = function (app, client)
             console.error('Error during resource creation:', e);
             res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
         }
-
-        // var refreshedToken = null;
-        // try
-        // {
-        //     refreshedToken = token.refresh(jwtToken);
-        // }
-        // catch(e)
-        // {
-        //     console.log(e.message);
-        // }
     });
 
     app.post('/api/editresource', async (req, res, next) =>
     {
-        // incoming: UserID, ResourceID, NewType, NewStart, NewEnd
+        // incoming: UserID, ResourceID, Type, Location, Description, Start, End
         // outgoing: updatedResource || error
 
-        // const {UserID, ResourceID, jwtToken} = req.body;
-        const {UserID, ResourceID, NewType, NewStart, NewEnd} = req.body;
-
-        // try
-        // {
-        //     if (token.isExpired(jwtToken))
-        //     {
-        //         var r = {error:'The JWT is no longer valid', jwtToken: ''};
-        //         res.status(200).json(r);
-        //         return;
-        //     }
-        // }
-        // catch(e)
-        // {
-        //     console.log(e.message);
-        //     var r = {error:e.message, jwtToken: ''};
-        //     res.status(200).json(r);
-        //     return;
-        // }
+        const {UserID,ResourceID,Type,Location,Description,Start,End} = req.body;
 
         try
         {
             const db = client.db('Scheduler');
-            const currentUser = await db.collection('Users').findOne({_id:new ObjectId(UserID)});
-            const editResource = await db.collection('Resources').findOne({_id:new ObjectId(ResourceID)});
-            // console.log(editResource, ResourceID);
-            // console.log(currentUser, UserID);
+            const currentUser = await db.collection('Users').findOne({_id:ObjectId.createFromHexString(UserID)});
+            const editResource = await db.collection('Resources').findOne({_id:ObjectId.createFromHexString(ResourceID)});
 
-            // probably not needed, if its showing on the website it must exist?
             if (!editResource)
             {
                 return res.status(404).json({error:'Resource not found.'});
@@ -212,24 +143,11 @@ exports.setApp = function (app, client)
                 return res.status(403).json({error:'Unauthorized access to edit this resource.'});
             }
 
-            // allow duplicates types (names) for now
-            // const existingType = await db.collection('Resources').findOne({Type:NewType});
-            // if (existingType)
-            // {
-            //     return res.status(409).json({error:'Resource name already exists.'});
-            // }
-
             const updatedResource = await db.collection('Resources').findOneAndUpdate(
-            {_id:new ObjectId(ResourceID)},
-            {
-                $set:
-                {
-                    Type:NewType,
-                    Start:new Date(NewStart),
-                    End:new Date(NewEnd)
-                }
-            },
-            {returnOriginal:false});
+                {_id:ObjectId.createFromHexString(ResourceID)},
+                {$set:{Type,Location,Description,Start:new Date(Start),End:new Date(End)}},
+                {returnDocument:'after'});
+
             res.status(200).json(updatedResource);
         }
         catch (e)
@@ -237,15 +155,174 @@ exports.setApp = function (app, client)
             console.error('Error during resource update:', e);
             res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
         }
+    });
 
-        // var refreshedToken = null;
-        // try
-        // {
-        //     refreshedToken = token.refresh(jwtToken);
-        // }
-        // catch(e)
-        // {
-        //     console.log(e.message);
-        // }
+    app.post('/api/deleteresource', async (req, res, next) =>
+    {
+        // incoming: UserID, ResourceID
+        // outgoing: deletedResource || error
+
+        const {UserID,ResourceID} = req.body;
+
+        try
+        {
+            const db = client.db('Scheduler');
+            const currentUser = await db.collection('Users').findOne({_id:ObjectId.createFromHexString(UserID)});
+            const deleteResource = await db.collection('Resources').findOne({_id:ObjectId.createFromHexString(ResourceID)});
+
+            if (!deleteResource)
+            {
+                return res.status(404).json({error:'Resource not found.'});
+            }
+
+            if (!currentUser.IsAdmin && deleteResource.UserID.toString() !== UserID)
+            {
+                return res.status(403).json({error:'Unauthorized access to delete this resource.'});
+            }
+
+            const deletedResource = await db.collection('Resources').findOneAndDelete(
+                {_id:ObjectId.createFromHexString(ResourceID)},
+                {returnDocument:'before'});
+
+            res.status(200).json(deletedResource);
+        }
+        catch (e)
+        {
+            console.error('Error during resource deletion:', e);
+            res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
+        }
+    });
+
+    app.post('/api/addreservation', async (req, res, next) =>
+    {
+        // incoming: UserID, ResourceID, Comment, Start, End
+        // outgoing: newReservation || error
+
+        const {UserID,ResourceID,Comment,Start,End} = req.body;
+
+        try
+        {
+            const db = client.db('Scheduler');
+            const existingReservation = await db.collection('Reservations').findOne(
+            {
+                ResourceID:ObjectId.createFromHexString(ResourceID),
+                $or:
+                [
+                    {Start:{$lt:new Date(End)},End:{$gt:new Date(Start)}},
+                    {Start:{$gte:new Date(Start),$lte:new Date(End)}},
+                    {End:{$gte:new Date(Start),$lte:new Date(End)}}
+                ]
+            });
+
+            if (existingReservation)
+            {
+                return res.status(409).json({error:'Reservation overlaps with existing reservation.'});
+            }
+
+            const insertedReservation = await db.collection('Reservations').insertOne(
+            {
+                UserID:ObjectId.createFromHexString(UserID),
+                ResourceID:ObjectId.createFromHexString(ResourceID),
+                Comment,Start:new Date(Start),End:new Date(End)
+            });
+
+            const newReservation = await db.collection('Reservations').findOne({_id:insertedReservation.insertedId});
+            res.status(201).json(newReservation);
+        }
+        catch (e)
+        {
+            console.error('Error during reservation creation:', e);
+            res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
+        }
+    });
+    
+    app.post('/api/editreservation', async (req, res, next) =>
+    {
+        // incoming: UserID, ReservationID, Comment, Start, End
+        // outgoing: updatedReservation || error
+
+        const {UserID,ReservationID,Comment,Start,End} = req.body;
+
+        try
+        {
+            const db = client.db('Scheduler');
+            const currentUser = await db.collection('Users').findOne({_id:ObjectId.createFromHexString(UserID)});
+            const editReservation = await db.collection('Reservations').findOne({_id:ObjectId.createFromHexString(ReservationID)});
+
+            if (!editReservation)
+            {
+                return res.status(404).json({error:'Reservation not found.'});
+            }
+
+            const existingReservation = await db.collection('Reservations').findOne(
+            {
+                _id:{$ne:editReservation._id},
+                ResourceID:editReservation.ResourceID,
+                $or:
+                [
+                    {Start:{$lt:new Date(End)},End:{$gt:new Date(Start)}},
+                    {Start:{$gte:new Date(Start),$lte:new Date(End)}},
+                    {End:{$gte:new Date(Start),$lte:new Date(End)}}
+                ]
+            });
+
+            if (existingReservation)
+            {
+                return res.status(409).json({error:'Reservation overlaps with existing reservation.'});
+            }
+
+            if (!currentUser.IsAdmin && editReservation.UserID.toString() !== UserID)
+            {
+                return res.status(403).json({error:'Unauthorized access to edit this reservation.'});
+            }
+
+            const updatedReservation = await db.collection('Reservations').findOneAndUpdate(
+                {_id:ObjectId.createFromHexString(ReservationID)},
+                {$set:{Comment,Start:new Date(Start),End:new Date(End)}},
+                {returnDocument:'after'});
+
+            res.status(200).json(updatedReservation);
+        }
+        catch (e)
+        {
+            console.error('Error during reservation update:', e);
+            res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
+        }
+    });
+
+    app.post('/api/deletereservation', async (req, res, next) =>
+    {
+        // incoming: UserID, ReservationID
+        // outgoing: deletedReservation || error
+
+        const {UserID,ReservationID} = req.body;
+
+        try
+        {
+            const db = client.db('Scheduler');
+            const currentUser = await db.collection('Users').findOne({_id:ObjectId.createFromHexString(UserID)});
+            const deleteReservation = await db.collection('Reservations').findOne({_id:ObjectId.createFromHexString(ReservationID)});
+
+            if (!deleteReservation)
+            {
+                return res.status(404).json({error:'Reservation not found.'});
+            }
+
+            if (!currentUser.IsAdmin && deleteReservation.UserID.toString() !== UserID)
+            {
+                return res.status(403).json({error:'Unauthorized access to delete this reservation.'});
+            }
+
+            const deletedReservation = await db.collection('Reservations').findOneAndDelete(
+                {_id:ObjectId.createFromHexString(ReservationID)},
+                {returnDocument:'before'});
+
+            res.status(200).json(deletedReservation);
+        }
+        catch (e)
+        {
+            console.error('Error during reservation deletion:', e);
+            res.status(500).json({error:'Could not connect to the databsase. Please try again later.'});
+        }
     });
 }
