282332958ba95a79b24eb1ea1fcb54f3cab44c02
milan-ma <144814769+milan-ma@users.noreply.github.com>
2024-06-29 10:37:55 -0400

    Update PageTitle.js

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 function PageTitle()
 {
+    const redirectToLogin = () =>
+    {
+        window.location.href = '/';
+    }
+    
+    const redirectToRegister = () =>
+    {
+        window.location.href = '/register';
+    }
+    
     return(
-        <h1 id="title">Team 2 Scheduler</h1>
+        <h1 id="title">ReserveSmart</h1>
+        <input
+            type="submit"
+            id="registerButton"
+            className="buttons"
+            value="Login"
+            onClick={redirectToLogin}
+        />
+        <input
+            type="submit"
+            id="registerButton"
+            className="buttons"
+            value="Sign up"
+            onClick={redirectToRegister}
+        />
     );
 };
 
