6d24ce92fe87b0bf168b285e57f098187d91369d
milan-ma <144814769+milan-ma@users.noreply.github.com>
2024-07-01 13:18:33 -0400

    Update PageTitle.js

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -2,32 +2,9 @@ import React from 'react';
 
 function PageTitle()
 {
-    const redirectToLogin = () =>
-    {
-        window.location.href = '/';
-    }
-    
-    const redirectToRegister = () =>
-    {
-        window.location.href = '/register';
-    }
     
     return(
-        <h1 id="title">ReserveSmar</h1>
-        <input
-            type="submit"
-            id="registerButton"
-            className="buttons"
-            value="Login"
-            onClick={redirectToLogin}
-        />
-        <input
-            type="submit"
-            id="registerButton"
-            className="buttons"
-            value="Sign up"
-            onClick={redirectToRegister}
-        />
+        <h1 id="title">ReserveSmart</h1>
     );
 };
 
