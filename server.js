443c62c15dd43e969c2d8ac4b6a8e791db909a3e
Israel Charles <israelcharlesic10@gmail.com>
2024-07-04 15:03:02 -0400

    minor modification

--- server.js
+++ server.js
@@ -371,13 +371,13 @@ app.post('/api/showreservations', async(req, res) =>
     }
 })
 
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === 'production');
 {
-    app.use(express.static('frontend/build'))
+    app.use(express.static('frontend/build'));
     app.get('*',(req,res) =>
     {
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
-    })
+    });
 }
 
 app.listen(PORT,() =>
