8a7cb7ff10a2eb994d8079008f2cbaece5e388bd
mahica-devireddy <mahica.devireddy@gmail.com>
2024-07-22 10:17:07 -0400

    Create forgot_password.dart

---
+++ frontendMobile/lib/forgot_password.dart
@@ -0,0 +1,152 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:reserve_smart/main.dart';
+
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController _emailController = TextEditingController();
+
+  Future<void> sendPasswordResetEmail(BuildContext context) async {
+    const String forgotPasswordUrl = 'https://4331booking.com/api/auth/password/forgot';
+
+    final String email = _emailController.text;
+
+    if (email.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Please enter your email address.'),
+        ),
+      );
+      return;
+    }
+
+    final Map<String, String> forgotPasswordData = {
+      'email': email,
+    };
+
+    try {
+      final response = await http.post(
+        Uri.parse(forgotPasswordUrl),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(forgotPasswordData),
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Password reset email sent successfully.'),
+          ),
+        );
+        _emailController.clear();
+      } else {
+        final Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(jsonResponse['message'] ?? 'Unknown error'),
+          ),
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Failed to connect to the server. Please try again later.'),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(height: 300),
+            const Text(
+              'Forgot Your Password?',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 31, 41, 55),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Enter an email address to receive a password reset link.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black54,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 15),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(200.0),
+                  borderSide: const BorderSide(color: Color.fromARGB(255, 31, 41, 55), width: 2.0),
+                ),
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () {
+                  sendPasswordResetEmail(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(200.0),
+                  ),
+                  backgroundColor: const Color.fromARGB(255, 31, 41, 55),
+                ),
+                child: const Text(
+                  'Send Email',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Already have an account? Log in!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Color.fromARGB(255, 31, 41, 55),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
